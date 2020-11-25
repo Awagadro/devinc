56845608ce92d3075b8a6cf20b9fa9a0f81175f1
WIN-U0AGMKOLSIH\user <awagadrog@gmail.com>
2020-11-25 15:13:54 +0300

    no message

--- src/devinc/pre/mod01/Starter.java
+++ src/devinc/pre/mod01/Starter.java
@@ -1,26 +1,64 @@
 package devinc.pre.mod01;
 
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Starter {
+	static String open = "([<{";
+	static String close = ")]>}";
 
 	public static void main(String[] args) {
 		// консольное приложение, в которое вводиться с консоли строка и проверяется
 		// правильность расставления скобок(“[”,”]”,”{”,”}”,”<”,”>”,”(”,”)”)
 
-		String input = callScanner();
-		System.out.println("Строка: " + input);
-		
-		
-		
-		
+		String input = callScanner(); // вызов сканнера для ввода проверяемой строки
 
+		System.out.println("Проверка строки: " + input);
+
+		String filtered = input.chars() // извлекаем из строки скобки в отдельную строку
+				.filter(i -> isOpen((char) i) || isClose((char) i))
+				.mapToObj(i -> "" + (char) i)
+				.collect(Collectors.joining());
+		System.out.println(filtered);
+
+		System.out.println("Результат проверки: " + isBalanced(filtered, ""));
+
+	}
+
+	private static boolean isBalanced(String input, String open) {
+		// находим и удаляем соответствующие скобки методом рекурсии
+		if (input.isEmpty()) {
+			return open.isEmpty();
+		} else if (isOpen(input.charAt(0))) { // ищем открывающие скобки
+			return isBalanced(input.substring(1), input.charAt(0) + open); // и, если находим, добавляем в open
+		} else if (isClose(input.charAt(0))) {// ищем закрывающие скобки
+			return !open.isEmpty() && isMatching(open.charAt(0), input.charAt(0))// и, если находим, сравниваем с
+																					// последней открывающей из open
+					&& isBalanced(input.substring(1), open.substring(1)); // если нашли соответствие, удаляем пару
+																			// скобок и повторяем цикл
+		} else {
+			return isBalanced(input.substring(1), open); // не обязательно, отработает если предварительно не извлекать
+															// скобки из текста
+		}
+
+	}
+
+	private static boolean isMatching(char chO, char chC) {
+		return open.indexOf(chO) == close.indexOf(chC);
+	}
+
+	private static boolean isOpen(char ch) {
+		return open.indexOf(ch) != -1;
+	}
+
+	private static boolean isClose(char ch) {
+		return close.indexOf(ch) != -1;
 	}
 
 	private static String callScanner() {
 		System.out.println("Введите сстроку для проверки и нажмите <Enter> ");
 		Scanner scan = new Scanner(System.in);
-		String name = scan.next();
+		String name = scan.nextLine();
 		scan.close();
 		return name;
 
